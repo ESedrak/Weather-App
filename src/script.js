57115934c38024ddab378bb055a1d5a3d0c53f98
ESedrak <elizabeth_sedrak@hotmail.com>
2022-03-08 08:59:06 +1100

    Formatting Weather Forecast/API

--- src/script.js
+++ src/script.js
@@ -28,6 +28,15 @@ function formatDate() {
   return todayTime;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = "";
+  forecastHTML = forecastElement.innerHTML = forecastHTML;
+
+  forecastElement.innerHTML = ``;
+}
+
 let todayTime = document.querySelector("#todaysTime");
 todayTime.innerHTML = formatDate();
 
@@ -43,6 +52,13 @@ function handleSubmit(event) {
   event.preventDefault();
   let city = document.querySelector("#search-text-input").value;
   searchCity(city);
+}
+
+function getForecast(coordinates) {
+  let apiKey = "2fe015e63630ed57d2ca7047e4ab4479";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function showTemperature(response) {
@@ -75,6 +91,8 @@ function showTemperature(response) {
   iconElement.setAttribute("alt", description);
 
   celsiusTemperature = temperature;
+
+  getForecast(response.data.coord);
 }
 
 function searchLocation(position) {
